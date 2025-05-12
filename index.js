5d55a73309f27e140f32b9711f1c5eb6d1683651
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-05-12 17:06:24 +0200

    lab-done

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) throw new Error("EmptySortedList");
+    return this.items[this.length - 1]
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) throw new Error("EmptySortedList");
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    this.items.reduce((acc, number) => acc + number, 0)
+  }
 
   avg() {}
 }
